a0ae1ff1f3e20fe1f9f3a7ae8a7258402e595a2c
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-11-22 10:47:29 +0000

    New js slider configuration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,13 @@
-//Vanilla carousel slide
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselImages = document.querySelectorAll('.carousel-slide img');
+const slides = document.querySelectorAll('slide');
+const next = querySelector('#next');
+const prev = querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
 
-//Buttons carousel slide
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
-
-//Counter slide
-let  counter = 1;
-let size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-
-//Event listeners
-nextBtn = addEventListener('click',()=>{
-    if (counter >= carouselImages.lenght -1) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
-    counter++;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-});
-
-prevBtn = addEventListener('click',()=>{
-    if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
-    counter--;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-});
-
-carouselSlide.addEventListener('transitionend', () => {
-    if (carouselImages[counter].id === 'lastClone') {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length -2;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
+const nextSlide = () => {
+    //Get current/active image
+    const   active = document.querySelector ('.active');
+    //Remove current/active image
+    active.classList.remove('active');
 }
-if (carouselImages[counter].id === 'firstClone')  {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length -counter;
-    carouselSlide.style.transform = 'translateX(' +(-size * counter) + 'px)';
-}
-});
-
-window.addEventListener('resize', () => {
-    carouselSlide.style.transition = "none";
-    size = carouselImages[0].clientWidth;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
