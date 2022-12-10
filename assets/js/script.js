2bdc5183a1b21658c5a94a871a8fbaee8275828b
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-12-10 21:44:39 +0000

    last changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,25 +38,3 @@ function nextImage() {
   // carouselSlide[currentImageCounter].style.display = "block";
   carouselSlide[currentImageCounter].style.opacity = 1;
 }
-
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-links');
-//     const navLinks = document.querySelectorAll('.nav-links li');
-    
-//     burger.addEventListener('click', () => {
-//         //toggle nav
-//         nav.classList.toggle('nav-active');
-//         // animate links
-//         navLinks.forEach((link, index) => {
-//         if (link.style.animation) {
-//                 link.style.animation = '';
-//         } else {
-//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1.5}s`;
-//             }
-
-//     });
-// });
-// }
-
-// navSlide();
---
+++ assets/js/script2.js
@@ -0,0 +1,25 @@
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".bx bxs-chevron-down js-arrow arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
